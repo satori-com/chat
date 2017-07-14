29e5c2fd3cf55a4845fd61bb9ea32b1439445161
Lee Bailey <lbailey@mz.com>
2017-07-13 17:00:11 -0700

    adding message console to chat

--- src/components/room.jsx
+++ src/components/room.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router';
 import Countdown from 'react-cntdwn';
 import classNames from 'classnames';
 import uuid from 'uuid';
+import { Console } from '@satori-sdk/component-library';
 import MessageInput from './message-input';
 import MessageHistory from './message-history';
 import TypingIndicator from './typing-indicator';
@@ -25,11 +26,21 @@ class Room extends React.Component {
     this.botSelectorActivateHandler = this._onBotSelectorActivate.bind(this);
     this.botSelectorDeactivateHandler = this._onBotSelectorDeactivate.bind(this);
     this.botSelectorCompleteHandler = this._onBotSelectorComplete.bind(this);
+    this.toggleConsoleHandler = this._onToggleConsole.bind(this);
 
     this.state = {
       hamburgerActive: false,
       botSelectorVisible: false,
+      consoleActive: false,
     };
+  }
+
+  _onToggleConsole(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const { consoleActive } = this.state;
+    this.setState({ consoleActive: !consoleActive, hamburgerActive: false });
   }
 
   _onTyping() {
@@ -86,70 +97,85 @@ class Room extends React.Component {
     const chatUrl = getChatUrl(channelName);
 
     return (
-      <div className="roomPane">
-        <header className="roomPane-header">
-          <div
-            className={classNames('hamburgerMenu', { active: this.state.hamburgerActive })}
-            onClick={() => this.setState({ hamburgerActive: !this.state.hamburgerActive })}
-          >
-            <img
-              src={iconHamburgerMenu}
-              className="icon-hamburgerMenu"
-              alt="Chat Menu" />
+      <div>
+        <div className="roomPane">
+          <header className="roomPane-header">
+            <div
+              className={classNames('hamburgerMenu', { active: this.state.hamburgerActive })}
+              onClick={() => this.setState({ hamburgerActive: !this.state.hamburgerActive })}
+            >
+              <img
+                src={iconHamburgerMenu}
+                className="icon-hamburgerMenu"
+                alt="Chat Menu" />
+            </div>
+            <h2 className="roomPane-name">
+              {channel.get('topic')} ({onlineUsers.size + 1})
+            </h2>
+            {expiration &&
+              <div className="roomPane-countdown">
+                <Countdown
+                  targetDate={getExpirationDate(expiration)}
+                  format={{hour: 'hh', minute: 'mm', second: 'ss'}}
+                  timeSeparator={':'}
+                  leadingZero={true}
+                  onFinished={onExpiration} />
+                <img src={iconTimer} className="iconTimer" alt="" />
+              </div>}
+          </header>
+          <div className="hamburgerMenu-container">
+            <Link
+              to={`/channel/${channelName}/members`}
+              key={channelName}
+              className={classNames('hamburgerMenu-item', { active: this.state.hamburgerActive })}>
+              Participants
+            </Link>
+            <a
+              href="https://www.satori.com/apps/chat"
+              className={classNames('hamburgerMenu-item', { active: this.state.hamburgerActive })}
+              target="_blank"
+              rel="noopener noreferrer">
+              Learn More
+            </a>
+            <a
+              href="#console"
+              className={classNames('hamburgerMenu-item', { active: this.state.hamburgerActive })}
+              onClick={this.toggleConsoleHandler}>
+              Toggle message console
+            </a>
           </div>
-          <h2 className="roomPane-name">
-            {channel.get('topic')} ({onlineUsers.size + 1})
-          </h2>
-          {expiration &&
-            <div className="roomPane-countdown">
-              <Countdown
-                targetDate={getExpirationDate(expiration)}
-                format={{hour: 'hh', minute: 'mm', second: 'ss'}}
-                timeSeparator={':'}
-                leadingZero={true}
-                onFinished={onExpiration} />
-              <img src={iconTimer} className="iconTimer" alt="" />
-            </div>}
-        </header>
-        <div className="hamburgerMenu-container">
-          <Link
-            to={`/channel/${channelName}/members`}
-            key={channelName}
-            className={classNames('hamburgerMenu-item', { active: this.state.hamburgerActive })}>
-            Participants
-          </Link>
-          <a
-            href="https://www.satori.com/apps/chat"
-            className={classNames('hamburgerMenu-item', { active: this.state.hamburgerActive })}
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn More
-          </a>
+          <MessageHistory
+            history={history}
+            status={status}
+            user={user}
+            expiration={expiration}
+          />
+          <TypingIndicator
+            onlineUsers={onlineUsers}
+          />
+          <footer className="roomPane-footer">
+            <BotSelector
+              isVisible={botSelectorVisible}
+              onComplete={this.botSelectorCompleteHandler}
+              onDeactivate={this.botSelectorDeactivateHandler}
+            />
+            <MessageInput
+              shareText={chatUrl}
+              shareUrl={chatUrl}
+              onSendMessage={this.sendMessageHandler}
+              onTyping={this.typingHandler}
+              onBotSelectorActivate={this.botSelectorActivateHandler}
+              botSelectorActive={botSelectorVisible}
+            />
+          </footer>
         </div>
-        <MessageHistory
-          history={history}
-          status={status}
-          user={user}
-          expiration={expiration}
-        />
-        <TypingIndicator
-          onlineUsers={onlineUsers}
-        />
-        <footer className="roomPane-footer">
-          <BotSelector
-            isVisible={botSelectorVisible}
-            onComplete={this.botSelectorCompleteHandler}
-            onDeactivate={this.botSelectorDeactivateHandler}
+        {
+          this.state.consoleActive &&
+          <Console
+            className="console"
+            message={history.last()}
           />
-          <MessageInput
-            shareText={chatUrl}
-            shareUrl={chatUrl}
-            onSendMessage={this.sendMessageHandler}
-            onTyping={this.typingHandler}
-            onBotSelectorActivate={this.botSelectorActivateHandler}
-            botSelectorActive={botSelectorVisible}
-          />
-        </footer>
+        }
       </div>
     );
   }
